8685ffaf88790c7e400e9dbce3d3ac376efd2da4
salhadin <salassefa@gmail.com>
2026-01-24 22:04:46 +0300

    ooops i jjust dont what i do

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,8 +3,6 @@ import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
